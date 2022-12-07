e9daaaa440bebe45cd9ea803f6abded0ef9986ff
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:24 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -37,24 +37,5 @@ project "shaderc"
 		"./libshaderc_util/src/version_profile.cc"
 	}
 
- 	filter "system:windows"
+	filter "action:vs*"
 		disablewarnings { "4996", "4717" }
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
